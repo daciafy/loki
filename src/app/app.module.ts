be17115c0c4b5ec8ba261d69453779ac28b7489c
David Davis <davlloyddavis@gmail.com>
2019-07-05 12:20:43 -0500

    NGMaterial Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MaterialModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
 export class AppModule { }
